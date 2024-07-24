869e74efb82dfe0c285160a655e03e96bdde22c5
Drexxy <100047007+psxjtwr01@users.noreply.github.com>
2024-07-24 04:39:52 -0500

    Create MainEco.py

---
+++ MainEco.py
@@ -0,0 +1,12 @@
+import time
+import math
+import sys
+import colorama
+
+def debug(text,errlevl = 0):
+  if errorlevl == 0:
+    text= colorama.Fore.YELLOW + "[!]" colorama.Fore.RESET +text
+    for chars in text:
+      print(char,flush=true,end="")
+      time.sleep(0.1)
+      
